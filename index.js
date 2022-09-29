d6b96361c41c435c51eced9e320fde242ca0baad
OshaunB <oshaun1250@gmail.com>
2022-09-29 17:54:09 +0000

    problem 1 complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//Code your solutions in this file
+//Code your solutions in this files
+
+//const fiveToOneHundred = function() {
+    //}
+
+const fiveToOneHundred = () => {
+    for (let i = 5; i < 101 ; i ++ ){
+        console.log(i);
+    }
+}
+
+fiveToOneHundred()
+
