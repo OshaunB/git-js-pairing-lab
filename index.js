25ae57d73cfb05d7fdf05b788a0961c053b6567f
OshaunB <oshaun1250@gmail.com>
2022-09-29 18:12:40 +0000

    completed number 3

--- index.js
+++ index.js
@@ -8,11 +8,21 @@ const fiveToOneHundred = () => {
         console.log(i);
     }
 }
-fiveToOneHundred()
+// fiveToOneHundred()
 
 const multiplesOfThree = () => {
     for (let i = 3; i < 100; i+=3){
         console.log(i);
     }
 }
-multiplesOfThree()
+//multiplesOfThree()
+
+const multiplesOfThreeOrFive = () => {
+    for(let i = 0; i <= 100; i++){
+        if(i % 3 === 0 || i % 5 === 0){
+            console.log(i);
+        }
+    }
+}
+
+multiplesOfThreeOrFive();
